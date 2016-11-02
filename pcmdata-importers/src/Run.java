7afda76b9fd89d1e1f96c8bb298b32c266ec39c7
Slynus <lustucru86@gmail.com>
2016-11-02 16:16:03 +0100

    Convert to maven project

--- pcmdata-importers/src/Run.java
+++ pcmdata-importers/src/Run.java
@@ -1,8 +1,15 @@
+import org.opencompare.api.java.PCM;
 
 public class Run { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		PCM p = null;
+		
+		p.addFeature(null);
+		
+		// serializer in CSV or pcm format
 
 	}
 
