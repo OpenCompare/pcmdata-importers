fb9240087324afbc280ddfe2dcc973acdca7a08f
Slynus <lustucru86@gmail.com>
2016-11-09 17:41:45 +0100

    test getting started

---
+++ pcmdata-importers/src/wikipediaSelector/Main.java
@@ -0,0 +1,17 @@
+package wikipediaSelector;
+
+import java.io.IOException;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+
+	}
+
+}
