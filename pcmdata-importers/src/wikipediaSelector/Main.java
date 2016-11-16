c0e272b09f90e04dea4844f4ca856610ac3d072d
slynus <17005852@e206m06.istic.univ-rennes1.fr>
2016-11-16 18:04:02 +0100

    Add file walker

--- pcmdata-importers/src/wikipediaSelector/Main.java
+++ pcmdata-importers/src/wikipediaSelector/Main.java
@@ -1,16 +1,73 @@
 package wikipediaSelector;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.opencompare.api.java.PCM;
+import org.opencompare.api.java.PCMContainer;
+import org.opencompare.api.java.impl.io.KMFJSONExporter;
+import org.opencompare.api.java.impl.io.KMFJSONLoader;
+import org.opencompare.api.java.io.CSVExporter;
+import org.opencompare.api.java.io.PCMLoader;
 
 public class Main {
 
-	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		
 
+		/*
+		 * GettingStartedTest test = new GettingStartedTest(); try {
+		 * test.testGettingStarted(); } catch (IOException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+
+		try (Stream<Path> paths = Files.walk(Paths.get("input-pcm"))) {
+			paths.forEach(filePath -> {
+				if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".pcm")) {
+					System.out.println(filePath);
+
+					File pcmFile = new File(filePath.toString());
+
+					PCMLoader loader = new KMFJSONLoader();
+					List<PCMContainer> pcmContainers = null;
+					try {
+						pcmContainers = loader.load(pcmFile);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					for (PCMContainer pcmContainer : pcmContainers) {
+						// Get the PCM
+						PCM pcm = pcmContainer.getPcm();
+
+						Boolean thispcmisgood = true;
+
+						// now determine if the pcm is good
+
+						if (thispcmisgood) {
+							KMFJSONExporter pcmExporter = new KMFJSONExporter();
+							String pcmString = pcmExporter.export(pcmContainer);
+
+							Path p = Paths.get("output-pcm/" + filePath.getFileName());
+							try {
+								Files.write(p, pcmString.getBytes());
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							System.out.println("PCM exported to " + p);
+
+						}
+					}
+				}
+			});
+		}
 
 	}
 
