92ded77393e0c8f2268001fdc5335535aa92f655
Mathieu Acher <mathieu.acher@irisa.fr>
2016-11-24 12:13:21 +0100

    refactoring

--- pcmdata-importers/src/OMDBTest.java
+++ pcmdata-importers/src/OMDBTest.java
@@ -1,20 +1,133 @@
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.opencompare.api.java.PCM;
+import org.opencompare.api.java.PCMContainer;
+import org.opencompare.api.java.extractor.CellContentInterpreter;
+import org.opencompare.api.java.impl.PCMFactoryImpl;
+import org.opencompare.api.java.io.CSVLoader;
+import org.opencompare.api.java.io.PCMDirection;
+
+import data_omdb.OMDBMediaType;
 import data_omdb.OmdbtoProduct;
 
 
 public class OMDBTest {
 	
 	@Test 
-	public void test1() {
+	public void testOMDBFilms1() throws IOException, JSONException {
+		String m = new OmdbtoProduct().mkCSV(OMDBMediaType.MOVIE);
+		
+		BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter 
+				(new FileOutputStream("output/data_omdb_film.csv"), 
+						StandardCharsets.UTF_8));
+		writer.write(m);
+		writer.close();
+	}
 	
+	@Test 
+	public void testOMDBSeries1() throws IOException, JSONException {
+		String m = new OmdbtoProduct().mkCSV(OMDBMediaType.SERIES);
+		
+		BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter 
+				(new FileOutputStream("output/data_omdb_serie.csv"), 
+						StandardCharsets.UTF_8));
+		writer.write(m);
+		writer.close();
+	}
+	
+	@Test 
+	public void testOMDBEpisodes1() throws IOException, JSONException {
+		String m = new OmdbtoProduct().mkCSV(OMDBMediaType.EPISODE);
+		
+		BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter 
+				(new FileOutputStream("output/data_omdb_episode.csv"), 
+						StandardCharsets.UTF_8));
+		writer.write(m);
+		writer.close();
+	}
+	
+	@Test 
+	public void test1() throws IOException {
+			
 			// create the CSV
 			//new OmdbtoProduct().mk... 
+		
 			// read the CSV file
-			//PCM pcm = ...
+			File file = new File("output/data_omdb_episode.csv");
+			CSVLoader csvL = new CSVLoader(
+	                new PCMFactoryImpl(),
+	                new CellContentInterpreter(new PCMFactoryImpl()), 
+	                PCMDirection.PRODUCTS_AS_LINES);
+	        List<PCMContainer> pcmC = csvL.load(file);
+	        PCM pcm = pcmC.get(0).getPcm();
 			
 			// check some properties of "pcm"
-			//assertNotNull(pcm);
+			assertNotNull(pcm);			
+			assertEquals(93, pcm.getProducts().size());
+	}
+	
+	
+	@Test 
+	public void test3() throws IOException, JSONException {
+		System.err.println("" + 
+				new OmdbtoProduct().mkCSVs(Arrays.asList(OMDBMediaType.SERIES)));
+	}
+	
+	@Test 
+	public void test4() throws IOException, JSONException {
+		System.err.println("" + 
+				new OmdbtoProduct().mkCSVs(Arrays.asList(OMDBMediaType.MOVIE)));
+	}
+	
+	@Test 
+	public void test5() throws IOException, JSONException {
+		System.err.println("" + 
+				new OmdbtoProduct().mkCSVs(Arrays.asList(OMDBMediaType.EPISODE)));
+	}
+	
+	@Test 
+	public void test6() throws IOException, JSONException {
+		Map<OMDBMediaType, String> csvs = new OmdbtoProduct().mkCSVs(Arrays.asList(
+				OMDBMediaType.SERIES, 
+				OMDBMediaType.EPISODE,
+				OMDBMediaType.MOVIE
+				));
+		
+		CSVLoader csvL = new CSVLoader(
+                new PCMFactoryImpl(),
+                new CellContentInterpreter(new PCMFactoryImpl()), 
+                PCMDirection.PRODUCTS_AS_LINES);
+        List<PCMContainer> pcmC = csvL.load(csvs.get(OMDBMediaType.MOVIE));
+        PCM pcm = pcmC.get(0).getPcm();
+        assertNotNull(pcm);
+        assertEquals(33, pcm.getProducts().size());
+        
+        CSVLoader csvL2 = new CSVLoader(
+                new PCMFactoryImpl(),
+                new CellContentInterpreter(new PCMFactoryImpl()), 
+                PCMDirection.PRODUCTS_AS_LINES);
+        List<PCMContainer> pcmC2 = csvL2.load(csvs.get(OMDBMediaType.EPISODE));
+        PCM pcm2 = pcmC2.get(0).getPcm();
+        assertNotNull(pcm2);
+        assertEquals(44, pcm2.getProducts().size());
+        
+				
+		
 	}
 
 }
--- pcmdata-importers/src/data_omdb/OmdbtoProduct.java
+++ pcmdata-importers/src/data_omdb/OmdbtoProduct.java
@@ -1,13 +1,18 @@
 package data_omdb;
-import java.io.* ;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL ;
 import java.nio.charset.StandardCharsets ;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.json.* ;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class OmdbtoProduct {
 		
@@ -253,262 +258,62 @@ public class OmdbtoProduct {
 		return obj ;
 	}
 	
-	public static void main(String[] args) throws IOException, JSONException {
+	public String mkCSV(OMDBMediaType t) throws JSONException, IOException {
 		
 		
-		String films = "" ;
-		List<Product> film = new ArrayList<Product>() ;
-		String series = "" ;
-		List<Product> serie = new ArrayList<Product>() ;
-		String episodes = "" ;
-		List<Product> episode = new ArrayList<Product>() ;
+		String pdt = "" ;
+		List<Product> pdts = new ArrayList<Product>() ; // unuseful
+	
 		for(int i=944000;i<944100;i++)
 		{
-			
-			
+				
 			 Product p  = createProductFromJson(idToJson(i)) ;
-			 if(p != null)
+			 if (p != null)
 			 {
-				 
-				 //on regarde si c'est un film ou une s�rie
-				 if(p.Type.equals("movie")){
-					 films += ProductMovie(p, film) ;
-					 System.out.println("on est dans le " + i + "eme id film");
-				 }
-				 else if(p.Type.equals("series")) {
-					 series += ProductSerie(p, serie) ;
-					 System.out.println("on est dans le " + i + "eme id serie");
-				 }
-				 else if (p.Type.equals("episode")) {
-					 episodes += ProductSerie(p, episode) ;
-					 System.out.println("on est dans le " + i + "eme id episode");
-				 }	
-				 
+				 //on regarde si c'est un film ou une serie
+				 if(p.Type.equals(t.toString()))
+					 pdts.add(p);
+					 pdt += OMDBProductFactory.getInstance().mkProduct(p, t); 				 		 
 			 }
-			
 		}
-		
-		
-		// on �crit le string dans un fichier
-		Write_file.write_file("data_omdb_film.csv");
-		System.out.println(films);
-		Write_file.write_file("data_omdb_serie.csv");
-		System.out.println(series);
-		Write_file.write_file("data_omdb_episode.csv");
-		System.out.println(episodes);
-	//	System.out.println(film.size());
-//		System.out.println(film.get(74).Actors.size());
-			
+				
+		return pdt;		
 	}
 	
-	public static String ProductMovie(Product p, List<Product> film){
-		film.add(p) ;
-		String sortie = "" ;
-		 sortie  += "\""+ p.imdbID + "\";\"";
-		 sortie  += p.Title + "\";\"" ;
-		 sortie  += p.imdbRating + "\";\"";
-		 sortie  += p.imdbVotes + "\";\"";
-		 int cpt = 0 ;
-		 //affichage du genre du film parcours de la liste
-		 for(String g : p.Genre){
-			 if(cpt == 0){
-				sortie  +=  g ;
-				 cpt++ ;
-			 }
-			 else{
-				 sortie  += "," + g;
-			 }
-			 	
-		 }
-		 sortie  += "\";\"";
-		 
-		 sortie  += p.Director + "\";\"";
-		 sortie  += p.Writer + "\";\"";
-		 cpt = 0 ;
-		 for(String a : p.Actors){
-			 if(cpt == 0){
-				 sortie  += a;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + a;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 cpt = 0 ;
-		 for(String b : p.Language){
-			 if(cpt == 0){
-				 sortie  += b;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + b;
-			 }
-		 }
-		 sortie  += "\";\"";cpt = 0 ;
-		 for(String c : p.Country){
-			 if(cpt == 0){
-				 sortie  += c;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + c;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 sortie  += p.Year + "\";\"";
-		 sortie  += p.Metascore + "\";\"";
-		 sortie  += p.Poster + "\";\"";
-		 sortie  += p.Plot + "\";";
-		 
-		 
-
-		 String Newligne=System.getProperty("line.separator"); 
-		 sortie += Newligne;
-		 return sortie ;
-	}
 	
-	public static String ProductSerie (Product p , List<Product> serie){
-		 serie.add(p) ;
-		 String sortie = "" ;
-		 sortie  += "\""+ p.imdbID + "\";\"";
-		 sortie  += p.Title + "\";\"" ;
-		 sortie  += p.totalSeasons +"\";\"";
-		 sortie  += p.imdbRating + "\";\"";
-		 sortie  += p.imdbVotes + "\";\"";
-		 int cpt = 0 ;
-		 //affichage du genre du film parcours de la liste
-		 for(String g : p.Genre){
-			 if(cpt == 0){
-				sortie  +=  g ;
-				 cpt++ ;
-			 }
-			 else{
-				 sortie  += "," + g;
-			 }
-			 	
-		 }
-		 sortie  += "\";\"";
-		 
-		 sortie  += p.Director + "\";\"";
-		 sortie  += p.Writer + "\";\"";
-		 cpt = 0 ;
-		 for(String a : p.Actors){
-			 if(cpt == 0){
-				 sortie  += a;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + a;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 cpt = 0 ;
-		 for(String b : p.Language){
-			 if(cpt == 0){
-				 sortie  += b;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + b;
-			 }
-		 }
-		 sortie  += "\";\"";cpt = 0 ;
-		 for(String c : p.Country){
-			 if(cpt == 0){
-				 sortie  += c;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + c;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 sortie  += p.Year + "\";\"";
-		 sortie  += p.Metascore + "\";\"";
-		 sortie  += p.Poster + "\";\"";
-		 sortie  += p.Plot + "\";";
-		 
-		 
-
-		 String Newligne=System.getProperty("line.separator"); 
-		 sortie += Newligne;
-		 
-		 System.out.println(sortie);
-		 
-		 return sortie ;
-	}
-	
-	public static String ProductEpisode(Product p, List<Product> episode){
-		episode.add(p) ;
-		String sortie = "" ;
-		 sortie  += "\""+ p.imdbID + "\";\"";
-		 sortie  += p.Title + "\";\"" ;
-		 sortie  += p.seriesID + "\";\"";
-		 sortie  += p.Season + "\";\"";
-		 sortie  += p.Episode + "\";\"";
-		 sortie  += p.imdbRating + "\";\"";
-		 sortie  += p.imdbVotes + "\";\"";
-		 int cpt = 0 ;
-		 //affichage du genre du film parcours de la liste
-		 for(String g : p.Genre){
-			 if(cpt == 0){
-				sortie  +=  g ;
-				 cpt++ ;
-			 }
-			 else{
-				 sortie  += "," + g;
-			 }
-			 	
-		 }
-		 sortie  += "\";\"";
-		 
-		 sortie  += p.Director + "\";\"";
-		 sortie  += p.Writer + "\";\"";
-		 cpt = 0 ;
-		 for(String a : p.Actors){
-			 if(cpt == 0){
-				 sortie  += a;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + a;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 cpt = 0 ;
-		 for(String b : p.Language){
-			 if(cpt == 0){
-				 sortie  += b;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + b;
-			 }
-		 }
-		 sortie  += "\";\"";cpt = 0 ;
-		 for(String c : p.Country){
-			 if(cpt == 0){
-				 sortie  += c;
-				 cpt ++ ;
-			 }
-			 else{
-				 sortie  += "," + c;
-			 }
-		 }
-		 sortie  += "\";\"";
-		 sortie  += p.Year + "\";\"";
-		 sortie  += p.Metascore + "\";\"";
-		 sortie  += p.Poster + "\";\"";
-		 sortie  += p.Plot + "\";";
-		 
-
-		 
+	public Map<OMDBMediaType, String> mkCSVs(Collection<OMDBMediaType> omTypes) throws JSONException, IOException {
 		
-
-		 String Newligne=System.getProperty("line.separator"); 
-		 sortie += Newligne;
-		 return sortie ;
+		
+		Map<OMDBMediaType, String> omdbTypes2CSV = new HashMap<OMDBMediaType, String>();
+	
+		for(int i=944000;i<944100;i++)
+		{
+					
+			 Product p  = createProductFromJson(idToJson(i)) ;
+			 if (p != null)
+			 {
+				 //on regarde si c'est un film ou une serie
+				 String oType = p.Type;
+				 for (OMDBMediaType omType : omTypes) {
+					 if(oType.equals(omType.toString())) {
+						 String pdt = omdbTypes2CSV.get(omType);
+						 if (pdt == null)
+							 omdbTypes2CSV.put(omType, "");
+						 else {
+							 pdt += OMDBProductFactory.getInstance().mkProduct(p, omType);
+							 omdbTypes2CSV.put(omType, pdt);
+						 }
+					 }
+				}
+				  				 		 
+			 }
+		}
+				
+		return omdbTypes2CSV;		
 	}
+	
+
+
+
 	
 }
