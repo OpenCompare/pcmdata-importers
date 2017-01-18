7a15c01f42391a0692559927ba72b34b698cbfdc
mael <mael@dy33013.etu.univ-rennes1.fr>
2017-01-18 17:52:04 +0100

    usable .pcm in off_output/pcms

--- pcmdata-importers/src/data_off/Run2.java
+++ pcmdata-importers/src/data_off/Run2.java
@@ -9,14 +9,14 @@ public class Run2 {
 
 public static void main(String[] arg0) throws IOException, JSONException{
 		
-		OFFactsCSVCreator creator = new OFFactsCSVCreator();
-		
-		creator.createCSVFromCategory("en:candies", false);
-		//OFFPCMCreator.mkPCMFromCategory("en:beers");
-		
-		OFFStats.printStats();
-		
-		creator.close();
+//		OFFactsCSVCreator creator = new OFFactsCSVCreator();
+//		
+//		creator.createCSVFromCategory("en:candies", true);
+		OFFPCMCreator.mkPCMFromCategory("en:candies");
+//		
+//		OFFStats.printStats();
+//		
+//		creator.close();
 	}
 
 }
