31bab19a196198b47b33992757ae810f1b7d981a
Slynus <lustucru86@gmail.com>
2017-01-18 17:08:45 +0100

    Debut filtres mutants

--- pcmdata-importers/src/pcm_Filter/Main.java
+++ pcmdata-importers/src/pcm_Filter/Main.java
@@ -24,8 +24,8 @@ public class Main {
 
 	public static final boolean writefile = true;
 
-	public static final String inputpath = "input-pcm";
-	public static final String outputpath = "output-pcm/";
+	public static final String inputpath = "../../input-model";
+	public static final String outputpath = "../../output-model/";
 
 	// "input-pcm" testing inputs
 	// "../../input-model" prod inputs
@@ -42,7 +42,7 @@ public class Main {
 		Stream<Path> paths = Files.walk(Paths.get(inputpath));
 
 		paths.forEach(filePath -> {
-			if (Files.isRegularFile(filePath) && filePath.toString().endsWith("2.pcm")) {
+			if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".pcm")) {
 				System.out.println("> PCM imported from " + filePath);
 
 				File pcmFile = new File(filePath.toString());
